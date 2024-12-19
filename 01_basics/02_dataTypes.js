50a5a76e6ad9713630072e8fbb36ba6c462ef85c
Himanshu Singh <154426926+Himanshu1237@users.noreply.github.com>
2024-12-19 06:22:01 +0000

    Data Types in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+/* 
+ECMAScript 5 has a strict mode that results in cleaner JavaScript, with fewer unsafe features, more warnings, and more logical behavior. The normal (nonstrict) mode is sometimes called “sloppy mode.”
+ */
+
+// alert( 3 + 3) // we are using nodejs, not browser so alert gives a error here
+// Gives a textbox alert in the browser with the printed message above
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Encrid")
+
+
+let name = "Himanshu"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
